e307f72d72b8d5ce396a4b5de7afc9e43f59efdf
Helena Paiva <hellenapaiva@msn.com>
2022-03-19 16:51:52 -0300

    adicionado o javascript  para gerar senha aleatoriamente

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("teste")
+let sliderElement = document.querySelector('#slider');
+let buttonElement = document.querySelector("#button");
+let sizePassword = document.querySelector("#valueRange");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;':,./<>?";	
+let newPassword = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+
+}
